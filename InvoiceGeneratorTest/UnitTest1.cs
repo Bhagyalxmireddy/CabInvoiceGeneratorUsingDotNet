949fe5d83e62470454fa162935ce1e2f2c9424e1
Bhagi <puppyreddy1997@gmail.com>
2020-12-12 18:46:34 +0530

    [BhagyaLaxmi]ADDED:Added code to print welcome message

---
+++ InvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using CabInvoiceGenerater;
+
+namespace InvoiceGeneratorTest
+{
+    public class Tests
+    {
+
+        InvoicGenerator invoicGenerator;
+        [SetUp]
+        public void Setup()
+        {
+            invoicGenerator = new InvoicGenerator();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
