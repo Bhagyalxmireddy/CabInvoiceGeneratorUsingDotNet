ffd32501cd48e83f428a95faa574130959323f13
Bhagi <puppyreddy1997@gmail.com>
2020-12-12 19:05:58 +0530

    [Bhagyalaxmi]ADDED:Added code and testcases to calculate The Fare with given Distance and time

--- CabInvoiceGenerater/InvoicGenerator.cs
+++ CabInvoiceGenerater/InvoicGenerator.cs
@@ -6,6 +6,14 @@ namespace CabInvoiceGenerater
 {
     public class InvoicGenerator
     {
-       
+        public const double NORMAL_MIN_COST_PER_KILOMETER = 10.0;
+        public const int NORMAL_COST_PER_MINUTE = 1;
+        public const double MIMINUM_FARE = 5;
+        
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = distance * NORMAL_MIN_COST_PER_KILOMETER + time * NORMAL_COST_PER_MINUTE;
+            return Math.Max(totalFare, MIMINUM_FARE);
+        }
     }
 }
--- InvoiceGeneratorTest/UnitTest1.cs
+++ InvoiceGeneratorTest/UnitTest1.cs
@@ -14,9 +14,20 @@ namespace InvoiceGeneratorTest
         }
 
         [Test]
-        public void Test1()
+        public void givenDistanceAndTime_ShouldCalculateTheFare()
         {
-
+            double distance = 2.0;
+            int time = 5;
+            double result = invoicGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(result, 25);
+        }
+        [Test]
+        public void givenMinDistnceAndMinTime_ShouldReturnMinFare()
+        {
+            double distance = 0.2;
+            int time = 1;
+            double result = invoicGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(result, 5);
         }
     }
 }
