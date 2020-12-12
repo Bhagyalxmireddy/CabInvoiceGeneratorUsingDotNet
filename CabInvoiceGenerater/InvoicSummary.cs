53755e661ffe0b20b1ff98ffee3ceda91d8c1432
Bhagi <puppyreddy1997@gmail.com>
2020-12-12 20:04:09 +0530

    [Bhagyalaxmi]ADDED:Added InvoiceSummary Class and testcases to Calculate TotalFare and No.of rides

---
+++ CabInvoiceGenerater/InvoicSummary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerater
+{
+    public class InvoicSummary
+    {
+        public int numOfRides;
+        public double totalFare;
+        public double averageFare;
+
+        public InvoicSummary(int numOfRides, double totalFare)
+        {
+            this.numOfRides = numOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = this.totalFare / this.numOfRides;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is InvoicSummary)) return false;
+            InvoicSummary inputObject = (InvoicSummary)obj;
+            return this.numOfRides == inputObject.numOfRides && 
+                                      this.totalFare == inputObject.totalFare &&
+                                      this.averageFare == inputObject.averageFare;
+        }
+        public override int GetHashCode()
+        {
+            return this.numOfRides.GetHashCode() ^ this.totalFare.GetHashCode() ^ this.averageFare.GetHashCode();
+        }
+    }
+}
